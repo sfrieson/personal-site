a11326c834c3c17146259073e96466631e9e68ac
Steven Frieson <sfrieson@gmail.com>
2020-01-20 13:15:10 -0500

    ⬆️ Ohr Deployment

---
+++ docs/ohr/precache-manifest.76790e3491009ff8780735dd445e6d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d853037a19b85a1d6483ca477da52bc",
+    "url": "/ohr/index.html"
+  },
+  {
+    "revision": "8e5c2c81c2c4a4505d9a",
+    "url": "/ohr/static/css/main.b2910eb5.chunk.css"
+  },
+  {
+    "revision": "84431e544793025b6e56",
+    "url": "/ohr/static/js/2.80ca6e8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ohr/static/js/2.80ca6e8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e5c2c81c2c4a4505d9a",
+    "url": "/ohr/static/js/main.74fefe3b.chunk.js"
+  },
+  {
+    "revision": "ca52f35f43e2ce320fb6",
+    "url": "/ohr/static/js/runtime-main.15068ebd.js"
+  }
+]);
--- docs/ohr/service-worker.js
+++ docs/ohr/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.288eb1797ae9d7aba50b912a3661da8a.js"
+  "/ohr/precache-manifest.76790e3491009ff8780735dd445e6d56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ohr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
