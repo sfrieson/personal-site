fc9ec1d3ba4caf561559fdba0dd94cd5c3d0a575
Steven Frieson <sfrieson@gmail.com>
2021-02-02 18:27:53 -0500

    🐜 Color theme radios switch when media query switches

--- views/components/ThemeSwitcher/script.script.js
+++ views/components/ThemeSwitcher/script.script.js
@@ -1,12 +1,21 @@
 /* TODO Check what happens when this doesn't load */
 window.requestIdleCallback(() => {
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  if (prefersDark) {
-    document.getElementById('prefers-dark').checked = true;
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  const darkRadio = document.getElementById('prefers-dark');
+  const lightRadio = document.getElementById('prefers-light');
+  if (prefersDark.matches) {
+    darkRadio.checked = true;
   }
   const themeSwitcherRoot = document.getElementById('theme-switcher');
   themeSwitcherRoot.style.display = 'block';
+  prefersDark.addEventListener('change', (changeEvent) => {
+    if (changeEvent.target.matches) {
+      darkRadio.checked = true;
+    } else {
+      lightRadio.checked = true;
+    }
+  });
   import('/js/theme-switcher/index.js').then(({ default: themeSwitcher }) => {
-    themeSwitcher(themeSwitcherRoot, prefersDark);
+    themeSwitcher(themeSwitcherRoot, prefersDark.matches);
   });
 });
