43412215e6d21c28874f07b1e111d728e1cbb1b0
Steven Frieson <sfrieson@gmail.com>
2021-02-02 21:54:39 -0500

    ⚡️ Optimizes swash svg

--- views/components/Swash.jsx
+++ views/components/Swash.jsx
@@ -3,19 +3,11 @@ const React = require('react');
 function Swash() {
   return (
     <svg
-      className="swash"
-      role="presentation"
       xmlns="http://www.w3.org/2000/svg"
-      width="1920"
-      height="60"
       viewBox="0 0 1920 60"
       preserveAspectRatio="none"
     >
-      <path
-        id="Path_1"
-        data-name="Path 1"
-        d="M0,0H1920V60S1227,24,747,24,0,38,0,38Z"
-      />
+      <path d="M0 0h1920v60S1227 24 747 24 0 38 0 38z" />
     </svg>
   );
 }
